4a2fd6da7e014cf5165730c12b01633147330b1e
HugoSoub <hugosoubey@gmail.com>
2023-07-03 16:36:21 +0200

    exo1.1

--- src/projetaudio/pipelines/etl/nodes.py
+++ src/projetaudio/pipelines/etl/nodes.py
@@ -8,5 +8,24 @@ import numpy as np
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
 
     # Code de prétraitement des images
-    # ...
-    return df
+    empty_columns = df.columns[df.isnull().any()]
+    empty_rows = df[df[empty_columns].isnull().any(axis=1)]
+    
+    if not empty_columns.empty:
+
+        for col in empty_columns:
+            empty_row_indices = empty_rows.index[empty_rows[col].isnull()]
+
+            for row_index in empty_row_indices:
+                print(f"Erreur : La colonne '{col}' est vide dans la ligne {row_index}.")
+                print("Toutes les ligne ce dessus on été supprimé")
+
+        df = df.dropna()
+
+        return df
+
+    else:
+
+        print("Aucune colonne vide dans le DataFrame.")
+
+        return df

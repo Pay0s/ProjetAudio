17140aa619d46a1a67fb8487cb1d15733757f20b
HugoSoub <hugosoubey@gmail.com>
2023-07-03 15:47:04 +0200

    exo1

--- src/projetaudio/pipelines/etl/nodes.py
+++ src/projetaudio/pipelines/etl/nodes.py
@@ -2,3 +2,11 @@
 This is a boilerplate pipeline 'etl'
 generated using Kedro 0.18.10
 """
+import pandas as pd
+import numpy as np
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+
+    # Code de prétraitement des images
+    # ...
+    return df
--- src/projetaudio/pipelines/etl/pipeline.py
+++ src/projetaudio/pipelines/etl/pipeline.py
@@ -4,7 +4,14 @@ generated using Kedro 0.18.10
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import preprocess
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+                node(
+                func=preprocess,
+                inputs="source_datas",
+                outputs="post_traitement_data",
+                name="traitement"
+                )
+            ])
